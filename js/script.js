e9d10e688b7b319ee7fc72c148dfa9a6a5f657f2
Jay <j.adeyina@gmail.com>
2022-03-21 18:46:52 +0900

    Updated user information

--- js/script.js
+++ js/script.js
@@ -1,54 +1,30 @@
-const userProfile = document.querySelector(".overview");
-const repoList = document.querySelector(".repo-list");
+const profileInfo = document.querySelector(".overview");
 const username = "Hyuuga81";
 console.clear();
 
-//Async function to access github profile info
-const getProfile = async function () {
-    const res = await fetch (
-        `https://api.github.com/users/${username}`
-    );
-    const data = await res.json();
-    console.log(data);
-    getUserInfo(data);
+const getProfileInfo = async function () {
+    const infoRequest = await fetch(`https://api.github.com/users/${username}`);
+    const userData = await infoRequest.json();
+    console.log(userData);
+
+    displayUserInfo(userData);
 };
 
-getProfile();
+getProfileInfo();
 
-/*Function to display user information*/
-const getUserInfo = function (data) {
-    const div = document.createElement("div");
+const displayUserInfo = function (userData) {
+    const div = document.createElement("div"); 
     div.classList.add("user-info");
-    div.innerHTML = `
-    <figure>
-        <img alt="user avatar" src=${data.avatar_url} />
-    </figure>
-    <div>
-      <p><strong>Name:</strong> ${data.name}</p>
-      <p><strong>Bio:</strong> ${data.bio}</p>
-      <p><strong>Location:</strong> ${data.location}</p>
-      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div> 
-    `;
-    userProfile.append(div);
-    getRepos();
-}
+    div.innerHTML = `<figure>
+    <img alt="user avatar" src=${userData.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${userData.name}</p>
+    <p><strong>Bio:</strong> ${userData.bio}</p>
+    <p><strong>Location:</strong> ${userData.location}</p>
+    <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
+  </div> `;
 
-//FUnction to fetch your repos from github
-const getRepos = async function () {
-    const request = await fetch(`https://api.github.com/users/${username}/repos?sort=newest&per_page=100`);
-    const repoInfo = await request.json();
-    console.log(repoInfo);
-    displayRepos(repoInfo);
+  profileInfo.append(div);
 };
 
-//Function to display repo information
-const displayRepos = function (repos) {
-    for (const item of repos) {
-        const repoItem = document.createElement("li");
-        repoItem.classList.add("item");
-        repoItem.innerHTML = `<h3>${item.name}</h3>`;
-        repoList.append(repoItem);
-    }
-    
-};
